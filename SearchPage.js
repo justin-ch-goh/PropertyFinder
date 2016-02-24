5373c626d6fd585b2da6dd07b4704c8a752b5f3c
justin-ch-goh <justin.ch.goh@gmail.com>
2016-02-24 23:03:46 +0800

    added geolocation

--- SearchPage.js
+++ SearchPage.js
@@ -133,6 +133,22 @@ class SearchPage extends Component {
         console.log(this.state.searchString);
     }
 
+    onLocationPressed() {
+        navigator.geolocation.getCurrentPosition(
+            location => {
+                var search = location.coords.latitude + ',' + location.coords.longitude;
+                this.setState({ searchString: search });
+                var query = urlForQueryAndPage('centre_point', search, 1);
+                this._executeQuery(query);
+            },
+            error => {
+                this.setState({
+                    message: 'There was a problem with obtaining your location: ' + error
+                });
+            }
+        );
+    }
+
     render() {
         console.log('SearchPage.render');
 
@@ -162,7 +178,7 @@ class SearchPage extends Component {
                     <Text style={styles.buttonText}>Go</Text>
                 </TouchableHighlight>
             </View>
-            <TouchableHighlight style={styles.button}
+            <TouchableHighlight style={styles.button} onPress={this.onLocationPressed.bind(this)}
                 underlayColor='#99d9f4'>
                 <Text style={styles.buttonText}>Location</Text>
             </TouchableHighlight>
